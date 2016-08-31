036ecb915d8f11df2b2e21b0a85956a9abf6c652
minhthu6521 <minhthu6521@gmail.com>
2016-08-31 06:41:38 +0300

    Scratch

---
+++ js/portfolio.js
@@ -0,0 +1,18 @@
+var previousScroll = 0,
+    headerOrgOffset = $('#header').height();
+
+$('#header-wrap').height($('#header').height());
+
+$(window).scroll(function () {
+    var currentScroll = $(this).scrollTop();
+    if (currentScroll > headerOrgOffset) {
+        if (currentScroll > previousScroll) {
+            $('#header-wrap').slideUp();
+        } else {
+            $('#header-wrap').slideDown();
+        }
+    } else {
+            $('#header-wrap').slideDown();
+    }
+    previousScroll = currentScroll;
+});
